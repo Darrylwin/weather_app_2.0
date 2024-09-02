35eab54de1cbaf82d30d77d9b676579cfa59fb74
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-09-02 13:09:40 +0000

    fetch weather

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/models/weather_model.dart';
+import 'package:myapp/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _weatherService = WeatherService(
+    apiKey: "d70b5ec9cea71ce1decd63f85e10e74c",
+  );
+  Weather? _weather;
+
+  //fetch weather
+  _fetchWeather() async {
+    //get current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+    //any errors
+    catch (e) {
+      print(e);
+    }
+
+    //weather animation
+
+    //init state
+    @override
+    void initState() {
+      super.initState();
+
+      //fetch weather on startup
+      _fetchWeather();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
